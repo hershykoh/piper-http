1bb7942d36c64967924c22c7d739624408556635
NaftaliKohn <hershy@medloopus.com>
2025-02-09 19:30:25 -0500

    Update run.py

--- run.py
+++ run.py
@@ -4,45 +4,27 @@
 import subprocess
 import os
 import sys
-if sys.version_info.major >= 3:
-    from subprocess import CompletedProcess
-else:
-    # Add a polyfill to Python 2
-    class CompletedProcess:
+def run(*popenargs, **kwargs):
+    input = kwargs.pop("input", None)
+    check = kwargs.pop("handle", False)
 
-        def __init__(self, args, returncode, stdout=None, stderr=None):
-            self.args = args
-            self.returncode = returncode
-            self.stdout = stdout
-            self.stderr = stderr
+    if input is not None:
+        if 'stdin' in kwargs:
+            raise ValueError('stdin and input arguments may not both be used.')
+        kwargs['stdin'] = subprocess.PIPE
 
-        def check_returncode(self):
-            if self.returncode != 0:
-                err = subprocess.CalledProcessError(self.returncode, self.args, output=self.stdout)
-                raise err
-            return self.returncode
-
-    def sp_run(*popenargs, **kwargs):
-        input = kwargs.pop("input", None)
-        check = kwargs.pop("handle", False)
-        if input is not None:
-            if 'stdin' in kwargs:
-                raise ValueError('stdin and input arguments may not both be used.')
-            kwargs['stdin'] = subprocess.PIPE
-        process = subprocess.Popen(*popenargs, **kwargs)
-        try:
-            outs, errs = process.communicate(input)
-        except:
-            process.kill()
-            process.wait()
-            raise
-        returncode = process.poll()
-        if check and returncode:
-            raise subprocess.CalledProcessError(returncode, popenargs, output=outs)
-        return CompletedProcess(popenargs, returncode, stdout=outs, stderr=errs)
-
-    subprocess.run = sp_run
-    # ^ This polyfill allows it work on Python 2 or 3 the same way
+    process = subprocess.Popen(*popenargs, **kwargs)
+    try:
+        stdout, stderr = process.communicate(input)
+    except:
+        process.kill()
+        process.wait()
+        raise
+    retcode = process.poll()
+    if check and retcode:
+        raise subprocess.CalledProcessError(
+            retcode, process.args, output=stdout, stderr=stderr)
+    return retcode, stdout, stderr
 
 # If no args are provided, explain
 if len(sys.argv) < 3:
@@ -62,7 +44,7 @@ script_folder = os.path.dirname(os.path.realpath(__file__))
 download_script = os.path.join(script_folder, "download/download-piper-voices.py")
 
 # Download the model
-subprocess.run(['python', download_script, link, target_folder])
+run(['python', download_script, link, target_folder])
 
 # Join target_folder with the model name
 model_path = os.path.join(target_folder, "model.onnx")
@@ -70,8 +52,8 @@ model_path = os.path.join(target_folder, "model.onnx")
 # If the --speaker arg is provided, run the http server with the model and the speaker
 if len(sys.argv) > 3:
     speaker = sys.argv[3]
-    subprocess.run(['python', '-m', 'piper.http_server', '-m', model_path, '-s', speaker,'--cuda'])
+    run(['python', '-m', 'piper.http_server', '-m', model_path, '-s', speaker,'--cuda'])
     # sys.exit(0)
 else:
-    subprocess.run(['python', '-m', 'piper.http_server', '-m', model_path,'--cuda'])
+    run(['python', '-m', 'piper.http_server', '-m', model_path,'--cuda'])
     # sys.exit(0)
